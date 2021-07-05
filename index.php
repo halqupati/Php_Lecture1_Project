d7e65b6cd208b282810091ac9d5a4151928e1f2b
halqupati <hudurah737@gmail.com>
2021-07-05 23:36:28 +0300

    frist commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kown Lectures</title>
+
+	<style>
+		body{
+			background-image:url('image1.jpg'); 
+		}
+
+
+		button{
+			height: 30px;
+		}
+
+		a{
+			text-decoration-line: none;
+		}
+	</style>
+</head>
+<body>
+        <h1 style="text-align: center">Kown Solution website Template</h1>
+        <h2 style="text-align: center">Hello Everyone in Kown solution</h2>
+      <div style="text-align: center">
+	<button style="background-color:orange;border: none">
+        <a  style="padding: 20px;" href="http://www.localhost/kown1/login.php">Login</a>		
+	</button>
+	<button style="background-color:white;border: none">
+       <a  style="padding: 20px;" href="http://www.localhost/kown1/register.php">Register</a>	
+	</button>
+	<button style="background-color:green;border: none">
+       <a   style="padding: 20px;" href="http://www.localhost/kown1/contact.php">contact Us</a>		
+	</button>	
+</div>
+<div style="text-align: center">
+<img style="width: 30%;height: 30%;padding: 20px;border-radius: 20%" src="image2.gif">	
+</div>
+
+</body>
+</html>
